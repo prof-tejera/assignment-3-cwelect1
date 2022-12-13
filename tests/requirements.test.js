dc26a00fc7f327599655914f87b49b547f589286
Corbett Weinberg <cweinberg@hbs.edu>
2022-12-13 13:11:04 -0500

    Updated based on assignment-3.

--- tests/requirements.test.js
+++ tests/requirements.test.js
@@ -1,112 +1,89 @@
 /**
   This file is here to capture the requirements of this project:
   It is the basis for which tests will spawn from (likely after assignment 2).
+			
 
-  1. FIFO Queue for timers
-    a) Each timer can be in one of three states: running, completed, and not running. You will need a way to keep track of what state the timer is in, so that you can display it accordingly (see the image above)
-    b) During configuration, the user can remove any timer from the queue, so you will be supporting deleting
-    c) While the timer is running, you will need to either store or dynamically calculate which timer is active.
-    d) You don't want to clear the configurations as the timers are running. The user should be able to restart the entire workout at anytime
+  1. Persisting state
+      Our workout app has a pretty big problem, when the user refreshes the page we lose all configuration.
+			 We want to solve this by persisting state, so that if the page is reloaded or closed accidentally, we can restore the user to the same state.
+			 I like to think of it as we have two separate chunks of state we want to store.
+			 The first is the initial timer configuration and the second is the running state.
+			 For the timer configuration state we can store this in the URL.
+			 As the user changes the configuration, we want to update the url.
+			 We should only do this when the user has added/removed/moved the timer from configuration.
+			 We recommend that you don't update URL every time they enter an input, but instead have some sort of save button that would sync URL.
+			 Once the user has configured the workout and started the workout, we want to store the running state in local storage.
+			 We don't want to be doing a million writes to local storage, so I recommend that you think about how we can accomplish this with fewer writes.
+			 It doesn't have to restore to the latest millisecond, but it should be somewhat close (5-10 seconds) to what the workout was at after a reload/refresh of the page.
+			
 
-  2. Routing - use react-router
-    a) / - Home
-        - List of timers to be run for a workout. User should be able to remove a timer
-          How can we use QUEUE if user can remove any timer here
-        - The total time the workout will take
-        - A button to "Add" a new timer. This button brings the user to the /add screen
-        - Controls to Pause/Resume the workout
-        - Controls to reset the workout back to its initial state
-        - Controls to "fast-forward" - ends the current running timer and moves onto the next one
-    b) /add - Add Timer
-        - When user clicks "Add" from Home screen, they are routed to this page, where they can choose the type of timer and configure all inputs for each timer. After configuring, the user confirms and the timer is added to the list.
-        - The /add page should allow the user to configure any of the four timers (stopwatch, countdown, XY, and tabata)
-          - user can choose type of timer to add (dropdown?)
-          - configuration fields appear after selection
-            - Countdown
-              - Start time (max value?)
-            - StopWatch
-              - End Time (max time?)
-            - XY
-              - Start Time
-              - # rounds
-            - Tabata
-              - # Rounds
-              - Work Time
-              - Rest Time
-          - what's the best data structure to store timer config...object?Array?
-            - timer.type
-            - timer.start
-            - timer.end
-            - timer.work_time
-            - timer.rounds
-          - create added timer list (newest timer at top or bottom?)
-          - store in browser local storage?
-        - The user should be able to go back to the home page from here
-          - refresh state when returning home (so configured timers appear)
-    c) /docs  - doumentation
+  2. Workout history
+      We want to create a new screen that displays a list of previous workouts (create a new route and link to the history page in the navbar).
+			Once a workout has been completed, add this workout to the history and save it to local storage.
+			On the new history screen display all workouts completed and for each workout you should display some summary of all timers run and what the durations/rounds for each timer was.
+			
 
-  3. Deliverable
-    a) A user can configure (combination of any timers in any order) and execute a workout
-    b) All four timers must be functional: stopwatch, countdown, tabata, and XY.
-    c) Routing must be configured to support the home route (/) and add route (/add)
-    d) As you make modifications to your generic components, make sure to update documentation and prop-types.
+  3. Edit a timer
+      After the workout has been configured and the user is on the main run workout screen, add functionality that allows the user to edit any of the timer configurations (remember to update URL).
+			
 
-  4. Grading Rubric
-    a) A workout can be configured with any combination of timers
-    b) Final workout application should be bug free
-    c) DRY (do not repeat yourself). Try to make sure common code is shared and not copy/pasted
-    d) Console is free of warnings/errors
-    e) Deploy your application
+  4. Change the order of a timer in configured workout
+      After the workout has been configured and the user is on the main run workout screen, the user can move any timer to a different position in the queue.
+			This can be done a couple of ways.
+			You are welcome to use a drag and drop third party package or come up with something on your own (remember to update URL).
+			
 
- */
+  5. Display total time
+      After the workout has been configured and the user is on the main run workout screen, display the total workout time and count down from total time to zero (when workout is complete) once the workout has been started.
+			
+
+  6. Add description to each timer
+      Add a description field to each time that the user can add when creating the timer and when editing the timer.
+			It should be displayed while the timer is running.
+			
+
+  7. Wrap app using react-error-boundary
+      If for any reason your workout app errors out, then you should handle this and present the user with an error message.
+			react-error-boundary package has a nice implementation of react "error boundaries" that you can use to handle this scenario.
+			
+
+Deliverables
+  Complete all features listed above
+  As always deploy your app
+*/
 
 /*
   Tasks
   1. Move calculations from DisplayTime to helper
   2. Style control/pages according to modern standards (Color wheel, etc...)
     - AddTimer
-
   3. Add indication that timer was added
   4. Bug: Add Timer button doesn't depress
   5. WorkoutView - correct state when staring the app (no workout, no buttons and proper text for user)
   6. Remove buttons from timers on workout page
   7. Add Start, Pause, and reset workout
   8. Add delete timer on workout page
-  9. 
-
+  9.			 
 */
 
 /*
 THINGS I want and need more info:
   1. Fade completed Timer - When next timer starts (a few seconds in), completed fades and dissapears (does this meet project req?)
       - https://stackoverflow.com/questions/15907079/css3-transition-fade-out-effect
-  2. 
-  3. 
+  2.	 
+  3.
   */
 
-/*import renderer from 'react-test-renderer';
-// import homepage (or App?)
+/*
 
-test('test starts when start button clicked', () => {
-  const component = renderer.create(
-    //<Link page="http://www.facebook.com">Facebook</Link>,
-  );
-  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+We will be grading based on the features listed above and overall code quality
 
-  // manually trigger the callback
-  renderer.act(() => {
-    tree.props.onMouseEnter();
-  });
-  // re-rendering
-  tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-
-  // manually trigger the callback
-  renderer.act(() => {
-    tree.props.onMouseLeave();
-  });
-  // re-rendering
-  tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-});*/
+  Persisting state (20pt)
+  Workout history (20pt)
+  Edit a timer (10pt)
+  Change the order of a timer in configured workout (10pt)
+  Display total time (10pt)
+  Add description to each timer (10pt)
+  Wrap app using react-error-boundary (10pt)
+  DRY and overall code quality (20pt)
+*/
